e1d7f369e9dc0070b769af0b2962e46d60fac85e
WAADALKATHERI\waad <w-2k2@hotmail.com>
2016-11-02 00:29:57 +0300

    Initial commit

---
+++ app/src/main/java/com/walkatheri/popularmovies/MoviesApiService.java
@@ -0,0 +1,12 @@
+package com.walkatheri.popularmovies;
+
+import retrofit.Callback;
+import retrofit.http.GET;
+
+/**
+ * Created by jose on 10/6/15.
+ */
+public interface MoviesApiService {
+    @GET("/movie/popular")
+    void getPopularMovies(Callback<Movie.MovieResult> cb);
+}
